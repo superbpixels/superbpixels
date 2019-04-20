95798c6af89778f9fdbad34b0007c7345eb6a3f7
superbpixels <45247882+superbpixels@users.noreply.github.com>
2019-04-20 14:29:04 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# superbpixels
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import "./css/reset.css";
+// import "./css/styles.css";
+// import "./css/header.css";
+
+// document.getElementById("app").innerHTML = `
+
+// `;
